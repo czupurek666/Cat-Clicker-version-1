cbc43bc05b34cb086fd05704c928c98c12555a11
karolina Kantorska <karolina.kantorska@gmail.com>
2018-11-25 13:08:04 +0100

    event listener does not work

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,12 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
-const browserSync = require("browser-sync").create();
-var webserver = require('gulp-webserver');
 
-gulp.task("default", ["styles"], function() {
-  gulp.watch("sass/**/*.scss", ["styles"]);
-
-  browserSync.init({
-    server: "./"
-  });
-});
 
 gulp.task("styles", function() {
   gulp
     .src("sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(
-      autoprefixer({
-        browsers: ["last 2 versions"]
-      })
-    )
-    .pipe(gulp.dest("./css"))
-    .pipe(browserSync.stream());
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(gulp.dest("./css"));
 });
-
-
---
+++ js/app.js
@@ -0,0 +1,29 @@
+//el.addEventListener('click', catClick);
+//element.addEventListener(event, function, useCapture);
+
+
+
+
+// const catPhoto = document.getElementsByClassName('cat_photo');
+// const button = document.getElementById('btn');
+// function myFunction() {
+//     alert ('Hello World!');
+// };
+
+document.getElementById('cat_photo').addEventListener('click', function(){
+   alert('Hello World!');
+});
+// document.getElementsByClassName('btn').addEventListener('click', catClick);
+//  function catClick(){
+//    alert('click');
+//  };
+//function catClick() {
+//  let count = 0;
+
+//  return function () {
+//    count += 1;
+//    return count;
+//  };
+//}
+
+//let clickCounter = catClick();
